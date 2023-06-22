457cadb889b805647036481c7480fb0a9aa53f4d
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 18:46:20 +0530

    Wrote the code to pass Second Test Case

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 module.exports = class StringCalculator {
     constructor() {}
-
     calculate(numberString = '', delimiter = ',') {
         let sum = 0;
         this.convertString(numberString, delimiter).forEach((number) => {
             sum += +number;
         });
-
-        
         return sum;
     }
-
+    //updated so as two pass one element as well
     convertString(string, delimiter) {
         return string.split(delimiter); // Split the string by the specified delimiter
     }
-
 };
