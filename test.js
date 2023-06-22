eba51c559acdc3f9d153f0764ee4ec96292c8df2
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 18:44:59 +0530

    Wrote Second Test Case

--- test.js
+++ test.js
@@ -8,10 +8,21 @@ describe('TestTDD', () => {
         beforeEach(() => {
             this.stringCalc = new StringCalculator();
         });
-        //First Test Case
+        //First Task 
         it('Calculate empty string', () => {
             // Test case: Calculate the sum of an empty string
             assert.strictEqual(this.stringCalc.calculate(''), 0);
         });
+        //Second Task
+        it('Calculate simple string with one element 1', () => {
+            // Test case: Calculate the sum of a string with one element
+            assert.strictEqual(this.stringCalc.calculate('1'), 1);
+        });
+
+        it('Calculate simple string with one element 2', () => {
+            // Test case: Calculate the sum of a different string with one element
+            assert.strictEqual(this.stringCalc.calculate('9'), 9);
+        });
+
    });
 });
