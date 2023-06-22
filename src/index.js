e3d4ed0073fdbd417f0bba33126b7af757fc8a8b
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 18:56:36 +0530

    Refactored to pass thrid test case

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ module.exports = class StringCalculator {
     constructor() {}
     calculate(numberString = '', delimiter = ',') {
         let sum = 0;
+        //this will make sure that two elements is accepted and summed!
         this.convertString(numberString, delimiter).forEach((number) => {
             sum += +number;
         });
         return sum;
     }
     //updated so as two pass one element as well
+
     convertString(string, delimiter) {
         return string.split(delimiter); // Split the string by the specified delimiter
     }
