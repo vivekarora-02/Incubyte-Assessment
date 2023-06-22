620db3b34dd909c169a1b5edc24635dee7fcaf57
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 22:08:47 +0530

    Wrote test case for negative numbers

--- test.js
+++ test.js
@@ -53,5 +53,11 @@ describe('TestTDD', () => {
             // Test case: Calculate the sum of a string with positive numbers and zero
             assert.strictEqual(this.stringCalc.calculate('1,2,3,0,4,5'), 15);
         });
+        
+        it('should throw an exception when negative numbers are present', () => {
+            // Test case: Validate that an exception is thrown when negative numbers are present
+            expect(() => this.stringCalc.calculate('1,-2,3')).to.throw('Error: Negative numbers not allowed!');
+        });
+
    });
 });
