63cbaadbca178834ac76277062e193c16410fe94
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 18:27:43 +0530

    Added the Tasks Required todo

---
+++ testCases.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const StringCalculator = require ('./src/index.js');
+const { expect } = require('chai');
+
+//Tasks
+describe('TestTDD', () => {
+    describe('Tasks:', () => {
+        beforeEach(() => {
+            this.stringCalc = new StringCalculator();
+        });
+
+        it('Calculate empty string', () => {
+            // Test case: Calculate the sum of an empty string
+            assert.strictEqual(this.stringCalc.calculate(''), 0);
+        });
+   });
+});
