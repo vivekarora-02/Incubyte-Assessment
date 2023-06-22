b07702aea4d155599e8cfbb797333bf0732a5ca3
vivekarora-02 <vivek.arora2024@gmail.com>
2023-06-22 22:15:45 +0530

    Refactored to handle all the given tasks including negative numbers

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ module.exports = class StringCalculator {
     constructor() {}
     calculate(numberString = '', delimiter = ',') {
         let sum = 0;
+        const negativeNumbers = []; // Array to store negative numbers
         //this will make sure that any numbers of elements (positive) is accepted and summed!
         //Calculate the sum of a string with positive numbers and zero
         this.convertString(numberString, delimiter).forEach((number) => {
+            this.validateNumber(number, negativeNumbers);
             sum += +number;
         });
+        if (negativeNumbers.length > 0) {
+            throw new Error(`Error: Negative numbers not allowed! ${negativeNumbers.join(', ')}`);
+        }
         return sum;
     }
+    
+
     //updated so as two pass one element as well
     //this will make sure that, Calculate the sum of a string with custom delimiter!
     convertString(string, delimiter) {
+        
         return string.split(delimiter); // Split the string by the specified delimiter
+    }
+
+    validateNumber(number, negativeNumbers) {
+        if (number < 0) {
+            negativeNumbers.push(number); // Add negative numbers to the array
+        }
     }
 };
